26506a05c7f252308b70c22af4d794fc732b598d
marcofpadeiro <marcofpadeiro@gmail.com>
2025-04-16 23:26:42 +0100

    Making it so other type of requests work as well (ex: POST with body)

--- src/main.rs
+++ src/main.rs
@@ -9,5 +9,5 @@ async fn main() {
     let args: Vec<String> = args().collect();
     let request = parse_file(args.get(1).unwrap()).unwrap();
 
-    println!("{}", perform_request(request).await.unwrap().text().await.unwrap());
+    println!("{:?}", perform_request(request).await.unwrap());
 }
--- src/request/executor.rs
+++ src/request/executor.rs
@@ -7,7 +7,11 @@ use super::model::Request;
 pub async fn perform_request(request: Request) -> Result<Response, Box<dyn Error>> {
     let client = reqwest::Client::new();
 
-    let builder = client.request(request.method.as_reqwest_method(), request.url);
+    let mut builder = client.request(request.method.as_reqwest_method(), request.url);
+
+    if let Some(body) = request.body {
+        builder = builder.body(body);
+    }
 
     let response = builder.send().await?;
 
